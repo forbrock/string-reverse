5fe18021ae33bc8db1b3baec9f561ee92da8fd13
Dmitrii Barsuk <69538187+forbrock@users.noreply.github.com>
2020-11-13 21:33:05 +0200

    Update StringReverse.java

--- src/main/java/StringReverse.java
+++ src/main/java/StringReverse.java
@@ -1,7 +1,9 @@
 
 class StringReverse {
 
-    public static String reverse(String arg) {
-        return "";
-    }
+    public static String reverse(String str) {
+		if (str.length() == 1) return str;
+		
+		return str.substring(str.length() - 1) + reverse(str.substring(0, str.length() - 1));
+	}
 }
